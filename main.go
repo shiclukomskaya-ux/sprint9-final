a6c34423a28346332d57dec8f75ac8410d26d5c4
Nastya <shic.lukomskaya@gmail.com>
2026-07-01 18:39:55 +0300

    Add generateRandomElements and maximum functions with tests

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 const (
@@ -11,12 +12,32 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size == 0 {
+		return []int{}
+	}
+	numbers := make([]int, size) // ваш код здесь
+	for i := 0; i < size; i++ {
+		numbers[i] = rand.Intn(SIZE)
+	}
+	return numbers
+
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+	if len(data) == 1 {
+		return data[0]
+	}
+	maximum := data[0] // ваш код здесь
+	for i := 1; i < len(data); i++ {
+		if data[i] > maximum {
+			maximum = data[i]
+		}
+	}
+	return maximum
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
